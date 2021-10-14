e12c8ef0d7819bb30c711f93adaa83e3427681aa
Валентин <valmak04@gmail.com>
2021-10-14 12:04:57 +0400

    0.1

--- src/main/java/RegularPolygon.java
+++ src/main/java/RegularPolygon.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 
 public class RegularPolygon {
 
     public static void main(String[] args) {
-        //write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        double s = Double.parseDouble(makeNormal(sc.nextLine()));
+        System.out.print(area(n, s));
+    }
+
+    private static Object area (int n, double side) {
+        if (n > 2) {
+            return (Math.pow(side, 2) * n) / (4 * Math.tan(Math.PI / n));
+        } else
+            return "NO";
+    }
+
+    private static String makeNormal (String s) {
+        return s.replace("," , ".");
     }
 }
